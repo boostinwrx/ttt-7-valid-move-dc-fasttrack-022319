e1b3b6785652b4c1f96e207823599aec2c11a1c4
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-06-09 04:00:34 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,6 @@
 def #valid_move?
-if position_taken?
-  pu
-else
+if #position_taken?
+  "false"
+else "true"
 end
 end
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
